3a0537d3e0c6588408de5102574ee8424fc3dd5e
Taan <tannguyen1220@gmail.com>
2022-11-28 08:19:27 +0700

    input text state

--- components/inputSection.js
+++ components/inputSection.js
@@ -1,17 +1,27 @@
-import React from 'react'
-import { TextInput } from 'react-native'
+import React, { useState } from 'react'
+import { TextInput, View, Text } from 'react-native'
 
 const InputSection = () => {
+    const [inputText, setInputText] = useState("")
     return (
-        <TextInput
-            style={{
-                height: 40,
-                borderColor: 'red',
-                borderWidth: 1,
-                margin: 10
-            }}
-            defaultValue="You can type whatever you like here"
-        />
+        <View style={{ backgroundColor: 'slate' }}>
+            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Type Some Things Below</Text>
+            <TextInput
+                style={{
+                    height: 40,
+                    borderColor: 'red',
+                    borderWidth: 1,
+                    margin: 10,
+                    width: 700,
+                }}
+                placeholder="You can type whatever you like here"
+                onChangeText={textChange => setInputText(textChange)}
+                defaultValue={inputText}
+            />
+            <Text style={{ padding: 10, fontSize: 42 }}>
+                {inputText.split(' ').map((word) => word + ' 🍕 	<(￣︶￣)> ').join(' ')}
+            </Text>
+        </View>
     )
 }
 
